a5a3a6fcc040fc0edd967efb4691cc7bcd168e5c
Yurina Shin <yurinas@gmail.com>
2017-12-08 18:54:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.count == 0
   puts "The line is currently empty."
 else
-  katz_deli.each_index {|x| puts "The line is currently: #{x[0] + 1}. #{katz_deli[0] }} 
+  # katz_deli.each_index {|x| puts "The line is currently:}
 
 end
 end
@@ -13,6 +13,6 @@ end
 def take_a_number(katz_deli,name)
 katz_deli << name
 
-   katz_deli.each_with_index {|item,index| puts "Welcome, #{item}. You are number #{index + 1} in line."}
+ puts "Welcome, #{name}. You are number #{katz_deli.count + 1} in line."}
 
 end
