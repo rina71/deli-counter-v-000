2ade9117b8cbd1e7a67847023853d0701b5372b1
Yurina Shin <yurinas@gmail.com>
2017-12-08 18:43:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(katz_deli)
   if katz_deli.count == 0
   puts "The line is currently empty."
 else
-  katz_deli.each_index {|x| puts "The line is currently: #{x[0] + 1}. #{katz_deli[0]}"}
+  katz_deli.each_index {|x| puts "The line is currently: #{x[0] + 1}. #{katz_deli[0]} #{x[1] + 1}. #{katz_deli[1]} #{x[2] + 1}. #{katz_deli[2]} #{x[3] + 1}. #{katz_deli[3]} #{x[4] + 1}. #{katz_deli[4]} #{x[5] + 1}. #{katz_deli[5]} "
+}
 
 
 end
